c1b116f7fa2aef89d028e257da24a4c3a6bb33dc
Gianmarco Cariggi <gianmarco.cariggi@hotmail.it>
2021-06-09 12:21:14 +0200

    Fix dark mode and add Google Analytics

--- assets/js/darkmode.js
+++ assets/js/darkmode.js
@@ -1,22 +1,18 @@
 let imode = localStorage.getItem("isDark");
 
-let toogleStatus = document.getElementById('check-mode');
 let bodyElement = document.getElementsByTagName('body')[0];
 let navLogo = document.getElementsByClassName('nav-logo')[0];
 let cards = document.getElementsByClassName('card');
+let darklogo = document.getElementById('darkmodelogo')
 
-if(imode == "true"){
-  toogleStatus.checked = true;
-  darkmode()
-}
-
-function darkmode(){
-  if(toogleStatus.checked){
+function checkdark(){
+  if(localStorage.getItem("isDark") == "true"){
     bodyElement.className += " " + "darkmode";
     navLogo.className += " " + "darkmode";
     for(let i = 0; i < cards.length; i++){
       cards[i].className += " " + "alt-background";
     }
+    darklogo.src = "./assets/img/light_mode_white_24dp.svg"
     localStorage.setItem("isDark", true);
   }else{
     bodyElement.classList.remove('darkmode');
@@ -24,6 +20,27 @@ function darkmode(){
     for(let i = 0; i < cards.length; i++){
       cards[i].classList.remove('alt-background');
     }
+    darklogo.src = "./assets/img/dark_mode_white_24dp.svg"
+    localStorage.setItem("isDark", false);
+  }
+}
+
+function darkmode(){
+  if(localStorage.getItem("isDark") == "false"){
+    bodyElement.className += " " + "darkmode";
+    navLogo.className += " " + "darkmode";
+    for(let i = 0; i < cards.length; i++){
+      cards[i].className += " " + "alt-background";
+    }
+    darklogo.src = "./assets/img/light_mode_white_24dp.svg"
+    localStorage.setItem("isDark", true);
+  }else{
+    bodyElement.classList.remove('darkmode');
+    navLogo.classList.remove('darkmode');
+    for(let i = 0; i < cards.length; i++){
+      cards[i].classList.remove('alt-background');
+    }
+    darklogo.src = "./assets/img/dark_mode_white_24dp.svg"
     localStorage.setItem("isDark", false);
   }
 }
