24c3aa70a2f7fecbe25977635eb9201069c03817
Gianmarco Cariggi <34303206+giacar@users.noreply.github.com>
2026-02-12 23:09:26 +0100

    fix cname

--- vite.config.ts
+++ vite.config.ts
@@ -5,5 +5,5 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   plugins: [react()],
   // Ensures assets use relative paths, essential for GitHub Pages custom domains or subpaths
-  base: './', 
+  base: '/', 
 })
